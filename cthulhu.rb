e35ec32c415d274fb6e39109987e7744209ba3a8
Laura Khalil <zazenergy@gmail.com>
2012-07-11 10:41:44 -0700

    Improved the timing, spelling errors and text formatting.

--- cthulhu.rb
+++ cthulhu.rb
@@ -6,8 +6,10 @@ end
 
 #This is where the player begins the game
 def start()
-  puts "You are in the City of Arkham. There is something amiss. 
-  Should you explore the school first, or the town hall?"
+  puts <<MESSAGE
+  You are in the City of Arkham. There is something amiss. 
+  Should you explore the school first, or the town hall?
+MESSAGE
   
   prompt;  next_move=gets.chomp
   
@@ -21,13 +23,17 @@ def start()
 end
 
 def dead(why)
+  sleep 2 #seconds
   puts "#{why} Good job!"
   exit
 end
 
 def school()
-  puts "You walk in the school and notice the eerie silence that pervades. The classrooms are empty and you see a light at the end of the hall 
-  that seems to lead to a dark cellar."
+  puts <<MESSAGES
+  You walk in the school and notice the eerie silence that pervades. 
+  The classrooms are empty and you see a light at the end of the hall 
+  that seems to lead to a dark cellar.
+MESSAGES
   
   sleep 2 #second
   
@@ -36,20 +42,23 @@ def school()
   prompt; next_move=gets.chomp
   
   if next_move.include? "cellar"
-    dead("You follow the light and head down the stairs. \n You encounter a secret passageway that must have been used by the fleeing school children. \n You follow it to the end and safely escape out of Arkham.")
+    sleep 1 #seconds
+    dead("You follow the light and head down the stairs. \nYou encounter a secret passageway that must have been used by the fleeing school children. \nYou follow it to the end and safely escape out of Arkham.")
     
   else next_move.include? "leave" or next_move.include? "school"
-    dice_roll(generated_number) #Need to fill this in with the dice role argument
+    sleep 1 #seconds
+    dice_roll("You go towards the exit.") #Need to fill this in with the dice role argument
       
   end  
 end
 
 def town_hall()
-  puts "You encounter the mayor who is all wiley eyed. His hair is on edges and you think you just saw something faintly resembling a tentacle pop out from underneath his tailcoat. \n He says, 'Come dear friend, I have some ancient texts to show you.' \n Do you follow him?"
+  puts "You encounter the mayor who is all wiley eyed. \nHis hair is on edge and you think you just saw something \nfaintly resembling a tentacle pop out from underneath his tailcoat. \n He says, 'Come dear friend, I have some ancient texts to show you.' \n Do you follow him?"
 
 prompt; next_move = gets.chomp
 
   if next_move.include? "yes"
+  sleep 2 #seconds
   dice_roll("He grabs you by the hand. 'Purrrrrfect. You will become the ultimate sacrifice!'")
   else dead("You leave this maniac to his own devices and flee Arkham!")
   end
@@ -61,12 +70,12 @@ def dice_roll(generated_number)
   
   sleep 2 #seconds
   puts "The dice roll begins, now!"
-  sleep 3
+  sleep 2
   generated_number=1 + rand(6)
-  if rand <= 4
-    dead("You rolled a #{generated_number}. Your might has been crushed and you will be swalled into the depths of Cthulhu's lair.")
+  if generated_number <= 4
+    dead("You rolled a #{generated_number}. Your might has been crushed \nand you will be swallowed up into the depths of Cthulhu's lair.")
   else 
-    dead("You have beaten the Elder god with the strength of your will.")
+    dead("You rolled a #{generated_number}. You have beaten the Elder god \nwith the strength of your will.")
   end
 end
 
